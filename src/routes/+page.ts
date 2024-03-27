046144444b787d90dd65aaedd268aec007fc4fba
mxmlndml <42516330+mxmlndml@users.noreply.github.com>
2024-03-27 22:43:35 +0100

    fix https

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,7 +2,7 @@ import type { PageLoad } from "./$types";
 
 export const load = (async ({ fetch, depends }) => {
   const fetchPlan = async () => {
-    const url = "http://mensa-api.mxmlndml.com/api/v1/canteens/187/meals";
+    const url = "https://mensa-api.mxmlndml.com/api/v1/canteens/187/meals";
 
     const response = await fetch(url);
     const plan = await response.json();
